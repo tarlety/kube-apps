156007341c073c0799431d3dc70505465940a533
tarlety <tarlety@users.noreply.github.com>
2019-05-03 22:40:34 +0800

    app-gitea always uses postgres.

--- app/gitea/10-configmap.sh
+++ app/gitea/10-configmap.sh
@@ -3,27 +3,9 @@
 NODE_SSH_PORT=${NODE_SSH_PORT:-22}
 echo NODE_SSH_PORT=$NODE_SSH_PORT
 
-DB_TYPE=${DB_TYPE:-postgres}
-echo DB_TYPE=$DB_TYPE
-
 ACTION=$1
 case $ACTION in
 	"on")
-		if [ ${DB_TYPE} == "sqlite3" ]; then
-cat <<EOF | kubectl create -f -
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: gitea-env
-  namespace: app-gitea
-data:
-  RUN_MODE: "prod"
-  SSH_DOMAIN: "gitea.${DOMAIN}"
-  SSH_PORT: "${NODE_SSH_PORT}"
-  ROOT_URL: "https://gitea.${DOMAIN}"
-EOF
-		fi
-		if [ ${DB_TYPE} == "postgres" ]; then
 cat <<EOF | kubectl create -f -
 apiVersion: v1
 kind: ConfigMap
@@ -50,7 +32,6 @@ data:
   POSTGRES_USER: "gitea"
   PGDATA: "/var/lib/postgresql/data/pgdata"
 EOF
-		fi
 	;;
 	"off")
 		kubectl delete -n app-gitea configmap gitea-env

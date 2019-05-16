37b2fb757d4b0a5883f3deda51673e59a1ad6449
tarlety <tarlety@users.noreply.github.com>
2019-05-17 00:20:17 +0800

    a simple registry-proxy.

--- app/registry-proxy/20-deploy.sh
+++ app/registry-proxy/20-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 REGISTRY_VERSION=${REGISTRY_VERSION:-registry:2}
-REGISTRY_UI_VERSION=${REGISTRY_UI_VERSION:-hyper/docker-registry-web:v0.1.2}
 
 ACTION=$1
 case $ACTION in
@@ -29,7 +28,7 @@ spec:
             - name: REGISTRY_AUTH
               value: "none"
           ports:
-            - name: registry
+            - name: web
               containerPort: 5000
               protocol: TCP
           volumeMounts:
@@ -41,20 +40,6 @@ spec:
             - mountPath: "/etc/docker/registry/config.yml"
               name: config
               subPath: config.yml
-        - image: ${REGISTRY_UI_VERSION}
-          name: registry-proxy-ui
-          imagePullPolicy: IfNotPresent
-          env:
-            - name: REGISTRY_URL
-              value: "http://localhost:5000/v2"
-            - name: REGISTRY_NAME
-              value: "zerus registry proxy"
-            - name: REGISTRY_AUTH_ENABLED
-              value: "false"
-          ports:
-            - name: web
-              containerPort: 8080
-              protocol: TCP
       volumes:
       - name: data
         persistentVolumeClaim:
--- app/registry-proxy/40-svc.sh
+++ app/registry-proxy/40-svc.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-NODE_PORTS=${NODE_PORTS:-5000}
-NODE_REGISTRY_PORT=$(echo $NODE_PORTS | { read NODE_REGISTRY_PORT NODE_REST_PORTS ; echo $NODE_REGISTRY_PORT ; })
-
 ACTION=$1
 case $ACTION in
 	"on")
@@ -15,29 +12,14 @@ metadata:
 spec:
   ports:
     - name: web
-      port: 8080
-      protocol: TCP
-  selector:
-    app: registry-proxy
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: registry
-  namespace: app-registry-proxy
-spec:
-  type: NodePort
-  ports:
-    - name: registry
       port: 5000
-      nodePort: ${NODE_REGISTRY_PORT}
       protocol: TCP
   selector:
     app: registry-proxy
 EOF
 		;;
 	"off")
-		kubectl delete -n app-registry-proxy svc registry web
+		kubectl delete -n app-registry-proxy svc web
 		;;
 	*)
 		echo $(basename $0) on/off

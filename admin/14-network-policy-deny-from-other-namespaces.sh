b5dc415d64c3dd03b9b0f914c0fd8086fcddec45
tarlety <tarlety@users.noreply.github.com>
2019-07-15 17:57:13 +0800

    disable network policy before knowing how to troubleshoot. again.

--- admin/14-network-policy-deny-from-other-namespaces.sh
+++ admin/14-network-policy-deny-from-other-namespaces.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 echo "DISABLE IT BEFORE HAS KNOWLEDGE TO TROUBLESHOOT NETWORKPOLICY ISSUE."
-exit 0
 
-APPNAME=$1
+#APPNAME=$1
 
-cat <<EOF | kubectl create -f -
-kind: NetworkPolicy
-apiVersion: networking.k8s.io/v1
-metadata:
-  namespace: app-${APPNAME}
-  name: deny-from-other-namespaces
-spec:
-  podSelector:
-    matchLables:
-  ingress:
-  - from:
-    - podSelector: {}
-EOF
+#cat <<EOF | kubectl create -f -
+#kind: NetworkPolicy
+#apiVersion: networking.k8s.io/v1
+#metadata:
+#  namespace: app-${APPNAME}
+#  name: deny-from-other-namespaces
+#spec:
+#  podSelector:
+#    matchLables:
+#  ingress:
+#  - from:
+#    - podSelector: {}
+#EOF
 

dae400b584185385a125a2e71c76fdb74d1a43e3
tarlety <tarlety@users.noreply.github.com>
2020-01-02 13:02:33 +0800

    hackmd app: upgrade from 1.3.1 to 1.4.1.

--- app/hackmd/20-deploy-hackmd.sh
+++ app/hackmd/20-deploy-hackmd.sh
@@ -3,7 +3,7 @@
 APPNAME=${APPNAME:-hackmd}
 REPLICAS=${REPLICAS:-1}
 
-HACKMD_VERSION=${HACKMD_VERSION:-hackmdio/hackmd:1.3.1}
+HACKMD_VERSION=${HACKMD_VERSION:-hackmdio/hackmd:1.4.1}
 
 ACTION=$1
 case $ACTION in

4840694934cfc3c412c056ffe27e16cb2514e1dd
tarlety <tarlety@users.noreply.github.com>
2020-06-23 09:32:50 +0800

    chore: upgrade nextcloud from 18.0.4 to 18.0.6; and upgrade nginx to 18.

--- app/nextcloud/22-deploy-nextcloud.sh
+++ app/nextcloud/22-deploy-nextcloud.sh
@@ -4,9 +4,9 @@ APPNAME=${APPNAME:-nextcloud}
 REPLICAS=${REPLICAS:-1}
 
 # https://hub.docker.com/_/nextcloud
-NEXTCLOUD_VERSION=${NEXTCLOUD_VERSION:-nextcloud:18.0.4-fpm}
+NEXTCLOUD_VERSION=${NEXTCLOUD_VERSION:-nextcloud:18.0.6-fpm}
 # https://hub.docker.com/_/nginx
-NGINX_VERSION=${NGINX_VERSION:-nginx:1.17.9}
+NGINX_VERSION=${NGINX_VERSION:-nginx:1.18.0}
 
 ACTION=$1
 case $ACTION in

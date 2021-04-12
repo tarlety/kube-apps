8b8f2b7740f92bcb19af976cd8998427a6f51410
tarlety <tarlety@users.noreply.github.com>
2021-04-12 11:28:41 +0800

    upgrade nextcloud to v21.0.1

--- app/nextcloud/22-deploy-nextcloud.sh
+++ app/nextcloud/22-deploy-nextcloud.sh
@@ -4,7 +4,7 @@ APPNAME=${APPNAME:-nextcloud}
 REPLICAS=${REPLICAS:-1}
 
 # https://hub.docker.com/_/nextcloud
-NEXTCLOUD_VERSION=${NEXTCLOUD_VERSION:-nextcloud:20.0.4-fpm}
+NEXTCLOUD_VERSION=${NEXTCLOUD_VERSION:-nextcloud:21.0.1-fpm}
 # https://hub.docker.com/_/nginx
 NGINX_VERSION=${NGINX_VERSION:-nginx:1.19.6}
 # https://hub.docker.com/_/alpine

08baabbb211b8b0f319306f4633606bbf403f5fc
tarlety <tarlety@users.noreply.github.com>
2021-11-04 11:26:50 +0800

    snipe-it: upgrade to 5.3.1.

--- app/snipe-it/21-deploy-snipe-it.sh
+++ app/snipe-it/21-deploy-snipe-it.sh
@@ -2,7 +2,7 @@
 
 APPNAME=${APPNAME:-snipe-it}
 
-SNIPEIT_VERSION=${SNIPEIT_VERSION:-snipe/snipe-it:v5.1.5}
+SNIPEIT_VERSION=${SNIPEIT_VERSION:-snipe/snipe-it:v5.3.1}
 # https://hub.docker.com/_/nginx
 NGINX_VERSION=${NGINX_VERSION:-nginx:1.19.6}
 # https://hub.docker.com/_/busybox

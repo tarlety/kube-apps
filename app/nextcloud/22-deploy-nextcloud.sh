c5169362e787277ac51eaa327acc43126413abbe
tarlety <tarlety@users.noreply.github.com>
2022-09-12 16:27:58 +0800

    upgrade nextcloud to v24.0.5-fpm

--- app/nextcloud/22-deploy-nextcloud.sh
+++ app/nextcloud/22-deploy-nextcloud.sh
@@ -4,7 +4,7 @@ APPNAME=${APPNAME:-nextcloud}
 REPLICAS=${REPLICAS:-1}
 
 # https://hub.docker.com/_/nextcloud
-NEXTCLOUD_VERSION=${NEXTCLOUD_VERSION:-nextcloud:24.0.3-fpm}
+NEXTCLOUD_VERSION=${NEXTCLOUD_VERSION:-nextcloud:24.0.5-fpm}
 # https://hub.docker.com/_/nginx
 NGINX_VERSION=${NGINX_VERSION:-nginx:1.22.0}
 # https://hub.docker.com/_/alpine

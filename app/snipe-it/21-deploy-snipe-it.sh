63574a80920d2e5786347471c1c2bcf8834dd2d1
tarlety <tarlety@users.noreply.github.com>
2022-10-07 09:47:37 +0800

    upgrade snipe-it v6.0.11 and dependencies.

--- app/snipe-it/21-deploy-snipe-it.sh
+++ app/snipe-it/21-deploy-snipe-it.sh
@@ -2,9 +2,9 @@
 
 APPNAME=${APPNAME:-snipe-it}
 
-SNIPEIT_VERSION=${SNIPEIT_VERSION:-snipe/snipe-it:v6.0.9}
+SNIPEIT_VERSION=${SNIPEIT_VERSION:-snipe/snipe-it:v6.0.11}
 # https://hub.docker.com/_/nginx
-NGINX_VERSION=${NGINX_VERSION:-nginx:1.22.0}
+NGINX_VERSION=${NGINX_VERSION:-nginx:1.23.1}
 # https://hub.docker.com/_/busybox
 BUSYBOX_VERSION=${BUSYBOX_VERSION:-busybox:1.34.1}
 
